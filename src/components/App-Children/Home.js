e04393c0f0373adb6b15b594dd0c726a3618a7f1
ClaireAdele <claire.castanet@outlook.com>
2021-02-11 19:24:07 +0000

    finally sorted the page container issue with CSS

--- src/components/App-Children/Home.js
+++ src/components/App-Children/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import devImg from './imgs/female developer 2-01.jpg'
+
 
 export default function Home() {
     return (
-        <div >
+        <div id="homepage" >
+            <img src={devImg} id="devImg"></img>
         </div>
     )
 }
